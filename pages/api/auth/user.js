9f09fed6dcb91fb9b094b0ca7e50a3f136cae4ae
jamisonwight <jmsnwight@gmail.com>
2023-07-03 09:07:03 -0400

    fix error on user api route

--- pages/api/auth/user.js
+++ pages/api/auth/user.js
@@ -5,39 +5,7 @@ export default async (req, res) => {
     if (req.method === 'GET') {
         const { token } = cookie.parse(req.headers.cookie)
 
-        if (!token) {import { instance } from '../../../lib/api'
-        import cookie from 'cookie'
-        
-        export default async (req, res) => {
-            if (req.method === 'GET') {
-                const { token } = cookie.parse(req.headers.cookie)
-        
-                if (!token) {
-                    return res.status(403).json({ message: 'not authorized' })
-                }
-        
-                // Set CORS headers
-                res.setHeader('Access-Control-Allow-Origin', 'https://morgansafetyservices.com')
-                res.setHeader('Access-Control-Allow-Methods', 'GET')
-                res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type')
-        
-                try {
-                    const response = await instance.get('/api/users/me', {
-                        headers: {
-                            Authorization: `Bearer ${token}`,
-                        },
-                    })
-        
-                    res.status(200).json({
-                        user: response.data.user,
-                        email: response.data.user.email,
-                        id: response.data.user.id,
-                    })
-                } catch (error) {
-                    res.status(403).json({ message: 'not authorized' })
-                }
-            }
-        }
+        if (!token) {
             return res.status(403).json({ message: 'not authorized' })
         }
 
