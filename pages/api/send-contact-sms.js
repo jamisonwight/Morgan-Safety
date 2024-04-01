cf7175467b1a55368c54d92902fe3f80ee78abb0
Jamison Wight <jamison@jdigital.studio>
2024-04-01 11:55:04 -0400

    Add dedicated number to SMS message

--- pages/api/send-contact-sms.js
+++ pages/api/send-contact-sms.js
@@ -1,5 +1,6 @@
-import api from '../../lib/clicksend'
 import 'dotenv/config'
+
+var api =  require('../../lib/clicksend')
 
 export default async (req, res) => {
     const { 
@@ -11,39 +12,52 @@ export default async (req, res) => {
         numberOfPeople, 
         comments 
     } = req.body
-  
+
     try {
-        const smsApi = new api.SMSApi(process.env.CLICKSEND_USERNAME, process.env.CLICKSEND_API_KEY)
-    
-        const message = new api.SMSMessage()
+        const dedicatedNumber = process.env.CLICKSEND_DEDICATED_NUMBER // From number for the SMS messages
+        const recipientNumbers = process.env.CLICKSEND_NUMBERS // All Numbers to send SMS Message
+        const recipientNumbersArray = recipientNumbers.split(',').map(number => number.trim()) 
 
-        message.from = 'Morgan Safety Services'
-        message.to = process.env.CLICKSEND_NUMBERS // Replace with your admin phone number
-        message.body = `
-            New Contact 
+        recipientNumbersArray.map(async (number) => {
+            try {
+                const smsApi = new api.SMSApi(process.env.CLICKSEND_USERNAME, process.env.CLICKSEND_PASSWORD)
+                smsApi.basePath = 'https://rest.clicksend.com/v3'
             
-            Name: ${name}
-            Email: ${email}
-            Phone: ${phoneNumber}
-            Form Type: ${formType}
-            ${(trainingType) ? 'Training Type: ' + trainingType : ''}
-            ${(numberOfPeople) ? 'Number Of People: ' + numberOfPeople : ''}
-            
-            Message: ${comments}
-        `;
-    
-        var smsCollection = new api.SmsMessageCollection()
+                const message = new api.SmsMessage()
 
-        smsCollection.messages = [message]
-        
-        smsApi.smsSendPost(smsCollection).then(function(response) {
-            console.log(response.body)
-            res.status(200).json({ success: true })
-        }).catch(function(err){
-            console.error(err.body)
+                message.from = dedicatedNumber
+                message.to = number // Phone number to recieve this SMS Message
+                message.body = `
+                    MORGAN SAFETY SERVICES
+                    
+                    ** New Contact **
+                    Name: ${name}
+                    Email: ${email}
+                    Phone: ${phoneNumber}
+                    Form Type: ${formType}
+                    ${(trainingType) ? 'Training Type: ' + trainingType : ''}
+                    ${(numberOfPeople) ? 'Number Of People: ' + numberOfPeople : ''}
+                    
+                    Message: ${comments}
+                `;
+            
+                var smsCollection = new api.SmsMessageCollection()
+                smsCollection.messages = [message]
+                
+                const response =  await smsApi.smsSendPost(smsCollection)
+                console.log('SMS response:', response.body)
+
+            } catch (error) {
+                console.log('SMS response:', error)
+            }
         })
+
+        // Response OK
+        res.status(200).json({ success: true })
     } catch (error) {
         console.error('SMS error:', error)
+
+        // Response Internal Error
         res.status(500).json({ success: false })
     }
 }
