5ad7b08708ce4ba90fa694ed7107a307f89cba3c
Jamison Wight <jamison@jdigital.studio>
2024-05-31 05:45:21 -0600

    Add promise ALL to dedicated numbers

--- pages/api/send-contact-sms.js
+++ pages/api/send-contact-sms.js
@@ -1,32 +1,30 @@
-import 'dotenv/config'
-
-var api =  require('../../lib/clicksend')
+import 'dotenv/config';
+var api = require('../../lib/clicksend');
 
 export default async (req, res) => {
-    const { 
-        name, 
-        email, 
-        phoneNumber, 
-        formType, 
-        trainingType, 
-        numberOfPeople, 
-        comments 
-    } = req.body
+    const {
+        name,
+        email,
+        phoneNumber,
+        formType,
+        trainingType,
+        numberOfPeople,
+        comments
+    } = req.body;
 
     try {
-        const dedicatedNumber = process.env.CLICKSEND_DEDICATED_NUMBER // From number for the SMS messages
-        const recipientNumbers = process.env.CLICKSEND_NUMBERS // All Numbers to send SMS Message
-        const recipientNumbersArray = recipientNumbers.split(',').map(number => number.trim()) 
+        const dedicatedNumber = process.env.CLICKSEND_DEDICATED_NUMBER; // From number for the SMS messages
+        const recipientNumbers = process.env.CLICKSEND_NUMBERS; // All Numbers to send SMS Message
+        const recipientNumbersArray = recipientNumbers.split(',').map(number => number.trim());
 
-        recipientNumbersArray.map(async (number) => {
+        const sendSmsPromises = recipientNumbersArray.map(async (number) => {
             try {
-                const smsApi = new api.SMSApi(process.env.CLICKSEND_USERNAME, process.env.CLICKSEND_PASSWORD)
-                smsApi.basePath = 'https://rest.clicksend.com/v3'
-            
-                const message = new api.SmsMessage()
+                const smsApi = new api.SMSApi(process.env.CLICKSEND_USERNAME, process.env.CLICKSEND_PASSWORD);
+                smsApi.basePath = 'https://rest.clicksend.com/v3';
 
-                message.from = dedicatedNumber
-                message.to = number // Phone number to recieve this SMS Message
+                const message = new api.SmsMessage();
+                message.from = dedicatedNumber;
+                message.to = number; // Phone number to receive this SMS Message
                 message.body = `
                     MORGAN SAFETY SERVICES
                     
@@ -40,22 +38,25 @@ export default async (req, res) => {
                     
                     Message: ${comments}
                 `;
-            
-                var smsCollection = new api.SmsMessageCollection()
-                smsCollection.messages = [message]
-                
-                const response =  await smsApi.smsSendPost(smsCollection)
+
+                var smsCollection = new api.SmsMessageCollection();
+                smsCollection.messages = [message];
+
+                const response = await smsApi.smsSendPost(smsCollection);
+                console.log('SMS response:', response);
             } catch (error) {
-                console.log('SMS response:', error)
+                console.log('SMS send error:', error);
             }
-        })
+        });
+
+        await Promise.all(sendSmsPromises);
 
         // Response OK
-        res.status(200).json({ success: true })
+        res.status(200).json({ success: true });
     } catch (error) {
-        console.error('SMS error:', error)
+        console.error('SMS error:', error);
 
         // Response Internal Error
-        res.status(500).json({ success: false })
+        res.status(500).json({ success: false });
     }
-}
+};
